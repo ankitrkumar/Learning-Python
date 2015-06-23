d336d449bfa783f41eb13ae4b2df27ce1e04ca05
ankitrkumar <ankitkumar1618@gmail.com>
2015-06-23 15:46:36 -0400

    Microblog flask

--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,7 +1,35 @@
+import flask
 from app import app
+from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 
 def index():
-	return "Hello, world!"
+	user = {'nickname' : 'Ankit'} #some user
+	posts = [
+		{
+			'author' : {'nickname' : 'Anuj'},
+			'body' : 'Working fellow now!'
+		},
+		{
+			'author' : {'nickname' : 'Susan'},
+			'body': 'some bitch'
+		}
+	]
+	return flask.render_template('index.html',
+		title = 'Test', 
+		user = user,
+		posts = posts)
+
+@app.route('/login', methods = ['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flask.flash('Login requested for OpenID="%s", remember_me=%s' %
+			(form.openid.data, str(form.remember_me.data)))
+		return flask.redirect('/index')
+	return flask.render_template('login.html', 
+		title ='Sign In',
+		form = form
+		)
