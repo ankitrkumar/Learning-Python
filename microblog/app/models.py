d46530ea585620559280b69901794d8ddf67980a
ankitrkumar <ankitkumar1618@gmail.com>
2015-06-28 23:42:51 -0400

    Profiles and avatars

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,10 +1,13 @@
 from app import db
+from hashlib import md5
 
 class User(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	nickname = db.Column(db.String(64), index = True, unique = True)
 	email = db.Column(db.String(120), index = True, unique =True)
 	posts = db.relationship('Post', backref = 'author', lazy ='dynamic')
+	about_me = db.Column(db.String(140))
+	last_seen = db.Column(db.DateTime)
 
 	def is_authenticated(self):
 		return True
@@ -21,8 +24,25 @@ class User(db.Model):
 		except NameError:
 			return str(self.id)
 
+	def avatar(self, size):
+		return 'http://www.gravatar.com/avatar/%s?d=mm&s=%d' % (md5(self.email.encode('utf-8')).hexdigest(), size)
+
+	@staticmethod
+	def make_unique_nickname(nickname):
+		if User.query.filter_by(nickname = nickname).first() is None:
+			return nickname
+		from random import randint
+		version = randint(1,9999)
+		while True:
+			new_nickname = nickname + str(version)
+			if User.query.filter_by(nickname = new_nickname).first() is None:
+				break
+			version = randint(1,9999)
+		return new_nickname
+
 	def __repr__(self):
 		return '<user %r>' % (self.nickname)
+
 
 class Post(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
--- microblog/app/views.py
+++ microblog/app/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from app import app, db, lm, oid
 from flask.ext.login import login_user, logout_user, current_user, login_required
-from .forms import LoginForm
+from .forms import LoginForm, EditForm
 from .models import User
+from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
@@ -50,6 +51,7 @@ def after_login(resp):
 		nickname = resp.nickname
 		if nickname is None or nickname == "":
 			nickname = resp.email.split('@')[0]
+		nickname = User.make_unique_nickname(nickname)
 		user = User(nickname = nickname, email = resp.email)
 		db.session.add(user)
 		db.session.commit()
@@ -64,11 +66,16 @@ def after_login(resp):
 @app.before_request
 def before_request():
 	g.user = current_user
+	if g.user.is_authenticated():
+		g.user.last_seen = datetime.utcnow()
+		db.session.add(g.user)
+		db.session.commit()
 
 @app.route('/logout')
 def logout():
 	logout_user()
 	return redirect(url_for('index'))
+
 @lm.user_loader
 def load_user(id):
 	return User.query.get(int(id))
@@ -87,3 +94,29 @@ def user(nickname):
 	return render_template('user.html',
 							user = user,
 							posts = posts)
+
+@app.route('/edit', methods = ['GET', 'POST'])
+@login_required
+def edit():
+	form = EditForm(g.user.nickname)
+	if form.validate_on_submit():
+		g.user.nickname = form.nickname.data
+		g.user.about_me = form.about_me.data
+		db.session.add(g.user)
+		db.session.commit()
+		flash('Profile has been updated successfully!')
+		return redirect(url_for('edit'))
+	else:
+		form.nickname.data = g.user.nickname
+		form.about_me.data = g.user.about_me
+	return render_template('edit.html', form = form)
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
+
